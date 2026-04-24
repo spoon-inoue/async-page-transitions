3f560d8db3f4f205f4aff10d613ee2a56df1cc2e
spoon-inoue <h-inoue@studio-spoon.co.jp>
2026-04-24 17:30:51 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,23 @@ export default defineConfig({
   server: {
     host: true,
   },
-  base: '/async-page-transitions/',
+  // base: '/async-page-transitions/',
+  base: '/',
   build: {
     // https://docs.astro.build/en/reference/configuration-reference/#buildformat
     format: 'preserve',
+  },
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/scripts/entry.js',
+          chunkFileNames: 'assets/scripts/chunk_[hash].js',
+          assetFileNames: 'assets/style/main[extname]',
+        },
+      },
+    },
   },
 })
